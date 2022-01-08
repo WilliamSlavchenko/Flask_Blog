9f1e7471d9f234f4500aa188725b830a8243cdea
WilliamSlavchenko <97297182+WilliamSlavchenko@users.noreply.github.com>
2022-01-08 15:05:03 +0300

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,22 +1,34 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from Flask_Blog.flaskblog.config import Config
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+app.config.from_object(Config)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+mail = Mail()
 
-from Flask_Blog.flaskblog.users.routes import users
-from Flask_Blog.flaskblog.posts.routes import posts
-from Flask_Blog.flaskblog.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from Flask_Blog.flaskblog.users.routes import users
+    from Flask_Blog.flaskblog.posts.routes import posts
+    from Flask_Blog.flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
